8a21d71877c53c7a2775bbed59aebcd3afe7ec86
Rahul Raj <raj.rahul686@gmail.com>
2025-08-21 20:22:06 +0530

    feat: integrate shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import UserTable from "./Components/Table/UserTable";
 
 function App() {
   return (
     <>
-      <h2>Table Ui multiSorting</h2>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h2 class="bg-sky-500">Table Ui multiSorting</h2>
+      <UserTable />
     </>
   );
 }
